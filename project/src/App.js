13513f98da8767dd8a5a1414b86eeecc3f90c1a7
Khaled Nabaheen <khalednabaheen8@gmail.com>
2023-05-17 11:54:06 +0300

    search component

--- project/src/App.js
+++ project/src/App.js
@@ -2,10 +2,12 @@ import './App.css'
 import JobList from "./components/JobList/JobList";
 
 import Navbar from './components/Navbar'
+import Search from './components/Search';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Search />
       <JobList />
     </div>
   );
